8af08a9450196fcdfe83d2ae0402254ed81afe4e
BastienVdp <bastienpp@hotmail.com>
2024-05-26 13:30:16 +0200

    add dist

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path, { resolve } from "path";
 import { glob } from "glob";
 
 export default {
@@ -6,7 +6,15 @@ export default {
   build: {
     outDir: path.join(__dirname, "dist"),
     rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, "src", "*.html")),
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        clubmed: resolve(__dirname, "src/projects/clubmed.html"),
+        doritos: resolve(__dirname, "src/projects/doritos.html"),
+        pattex: resolve(__dirname, "src/projects/pattex.html"),
+        sarahlandry: resolve(__dirname, "src/projects/sarah-landry.html"),
+        swiffer: resolve(__dirname, "src/projects/swiffer.html"),
+        tea: resolve(__dirname, "src/projects/tea.html"),
+      },
     },
   },
 };
